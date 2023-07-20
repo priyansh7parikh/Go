f57647dc4e9643420461b60d7986da873a7eb05a
Priyansh Parikh <parikh.priyansh@gmail.com>
2023-07-20 20:47:24 +0530

    Updated deck and main.go project

--- new project/deck.go
+++ new project/deck.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 //Create a New Type of 'deck'
 //which is 	a slice of strings
 type deck []string
 
 func newDeck() deck {
-
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamond", "Heart", "Clubs"}
@@ -18,12 +21,24 @@ func newDeck() deck {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
-
 	return cards
 }
 
 func (d deck) print() {
-	for _, card := range d {
-		fmt.Println(card)
+	for i, card := range d {
+		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+	// []string(d)
+}
+
+func (d deck) savetoFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- new project/main.go
+++ new project/main.go
@@ -1,7 +1,19 @@
 package main
 
 func main() {
+	//To create a new deck
 	cards := newDeck()
 
-	cards.print()
+	// cards.savetoFile("my_cards")
+	// fmt.Println(cards.toString())
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+	//Print all the values present in card slice/array
+	// cards.print()
+
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
 }
